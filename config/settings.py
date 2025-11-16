bc7ec7f4baa97f8686f0584be5fe1fe9852cc707
colin-ingraham <colinringraham@gmail.com>
2025-11-16 15:44:21 +0000

    Added os

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ndd(_=bcih-(+t&$n_mab$j&jm!^ef+hu1qvj7g+7qy9_+&lo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ingrahamc.pythonanywhere.com']
 
 
 # Application definition
@@ -116,7 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root') # Choose a simple, new directory name
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), # Assuming your images are in 'your_project/static/'
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
